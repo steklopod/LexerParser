17848a3e51f26d25c99962ef48816ede21276101
steklopod <steklopod@gmail.com>
2018-08-21 22:46:28 +0300

    parser, compiler added

--- src/main/scala/ru/steklopod/lexer/WorkflowToken.scala
+++ src/main/scala/ru/steklopod/lexer/WorkflowToken.scala
@@ -7,16 +7,15 @@ sealed trait WorkflowToken extends Positional
 case class IDENTIFIER(str: String) extends WorkflowToken
 case class LITERAL(str: String) extends WorkflowToken
 case class INDENTATION(spaces: Int) extends WorkflowToken
+case class EXIT() extends WorkflowToken
+case class READINPUT() extends WorkflowToken
+case class CALLSERVICE() extends WorkflowToken
+case class SWITCH() extends WorkflowToken
+case class OTHERWISE() extends WorkflowToken
+case class COLON() extends WorkflowToken
+case class ARROW() extends WorkflowToken
+case class EQUALS() extends WorkflowToken
+case class COMMA() extends WorkflowToken
 
-case object EXIT extends WorkflowToken
-case object READINPUT extends WorkflowToken
-case object CALLSERVICE extends WorkflowToken
-case object SWITCH extends WorkflowToken
-case object OTHERWISE extends WorkflowToken
-case object COLON extends WorkflowToken
-case object ARROW extends WorkflowToken
-case object EQUALS extends WorkflowToken
-case object COMMA extends WorkflowToken
-
-case object INDENT extends WorkflowToken
-case object DEDENT extends WorkflowToken
+case class INDENT() extends WorkflowToken
+case class DEDENT() extends WorkflowToken
